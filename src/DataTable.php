e2bccd79a6726f1dffdbe98cec8b73b28f987869
Sina <Sina Sharifzade>
2018-10-14 00:09:16 +0330

    Initial Commit

---
+++ src/DataTable.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Elegant\DataTables;
+
+use JsonSerializable;
+use Illuminate\Contracts\Support\Jsonable;
+use Illuminate\Contracts\Support\Arrayable;
+
+class DataTable implements JsonSerializable, Jsonable, Arrayable
+{
+    protected $draw;
+    protected $total;
+    protected $totalFiltered;
+    protected $data;
+
+    public function __construct($draw, $total, $totalFiltered, $data)
+    {
+        $this->draw = $draw;
+        $this->total = $total;
+        $this->totalFiltered = $totalFiltered;
+        $this->data = $data;
+    }
+
+    public function getTotal()
+    {
+        return $this->total;
+    }
+
+    public function getTotalFiltered()
+    {
+        return $this->totalFiltered;
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    public function toArray()
+    {
+        return [
+            'draw' => $this->draw,
+            'recordsTotal' => $this->total,
+            'recordsFiltered' => $this->totalFiltered,
+            'data' => $this->data,
+        ];
+    }
+
+    public function jsonSerialize()
+    {
+        return $this->toArray();
+    }
+
+    public function toJson()
+    {
+        return json_encode($this->toArray());
+    }
+}
---
+++ src/DataTableBuilder.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace Elegant\DataTables;
+
+use Illuminate\Http\Request;
+
+class DataTableBuilder implements JsonSerializable, Jsonable, Arrayable
+{
+    protected $source;
+    protected $request;
+    protected $addonColumns = [];
+
+    public function addSource($source)
+    {
+        $this->source = $source;
+
+        return $this;
+    }
+
+    public function addColumn($name, $value)
+    {
+        $this->addonColumns[] = ['name' => $name, 'value' => $value];
+
+        return $this;
+    }
+
+    protected function search($search, $column)
+    {
+        if ($search['regex']) {
+            $this->source->where($column['name'], 'REGEX', $search['value']);
+        } else {
+            $this->source->where($column['name'], 'LIKE', $search['value']);
+        }
+    }
+
+    protected function filter($search, $columns)
+    {
+        // global filter
+        if ('' != $search['value']) {
+            foreach ($columns as $column) {
+                if ($column['searchable']) {
+                    $this->search($search, $column);
+                }
+            }
+        }
+
+        // column specific filter
+        foreach ($columns as $column) {
+            if ('' != $search['value']) {
+                $this->search($column['search'], $column);
+            }
+        }
+    }
+
+    protected function sort($orders, $columns)
+    {
+        foreach ($orders as $order) {
+            $this->source->orderBy($columns[$order['column']]['name'], $order['dir']);
+        }
+    }
+
+    protected function paginate($start, $length)
+    {
+        $this->source->offset($start)->limit($length);
+    }
+
+    protected function compileValue($value, array $data)
+    {
+        if ($value instanceof Coluser) {
+            $value = $column['value'](...$data);
+        } elseif (view()->exists($column['value'])) {
+            $value = view($str, $data);
+        } else {
+            $value = $column['value'];
+        }
+    }
+
+    protected function setupSourceColumns(&$row, $model)
+    {
+        $row = $model->toArray() + $row;
+    }
+
+    protected function setupAddonColumns(&$row, $model)
+    {
+        foreach ($this->addonColumns as $column) {
+            $row[$column['name']] = $this->compileValue($column['value'], ['model' => $model]);
+        }
+    }
+
+    protected function setupColumns($model)
+    {
+        $row = [];
+
+        $this->setupSourceColumns($row, $model);
+        $this->setupAddonColumns($row, $model);
+
+        return $row;
+    }
+
+    protected function setupRows($models)
+    {
+        $rows = [];
+
+        foreach ($models as $model) {
+            $rows[] = $this->setupColumns($model);
+        }
+
+        return $rows;
+    }
+
+    public function setRequest(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function getRequest()
+    {
+        return $this->request ?? request();
+    }
+
+    public function build()
+    {
+        $request = $this->getRequest();
+
+        $draw = $request->input('draw', 1);
+
+        $start = $request->input('start', 0);
+        $length = $request->input('length', 10);
+
+        $columns = $request->input('columns', []);
+        $search = $request->input('search', []);
+        $order = $request->input('order', []);
+
+        $total = $this->source->getCountForPagination();
+
+        $this->filter($search, $columns);
+
+        $totalFiltered = $this->source->getCountForPagination();
+
+        $this->sort($order, $columns);
+        $this->paginate($start, $length);
+
+        $models = $this->source->get();
+
+        $data = $this->setupRows($models);
+
+        return new DataTable(
+            $draw, $total, $totalFiltered, $data
+        );
+    }
+}
